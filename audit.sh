be739ac57675a7c369b3d34df8e90587405f25e4
Andy Smith <termie@users.noreply.github.com>
2017-01-30 15:02:01 -0800

    add a migration helper, more audits (#36)
    * add a migration helper, more audits
    
    * ensure deps
    
    * add diff, faster deps
    
    * more audits and migrations
    
    * typo
    
    * don't audit json fixtures
    
    * check artifact output, skip some json
    
    * updated rename_yaml
    
    * switch to local var declarations
    
    * update golang version:

--- audit.sh
+++ audit.sh
@@ -19,7 +19,6 @@ white() {
   printf "${WHITE}$@${NC}"
 }
 
-
 green() {
   printf "${GREEN}$@${NC}"
 }
@@ -32,7 +31,6 @@ success() {
   green "✓\n"
 }
 
-
 # Check that we are not using some package
 check_not_using() {
   white "Checking not using $1... "
@@ -42,6 +40,7 @@ check_not_using() {
     --exclude-dir \.wercker \
     --exclude-dir \.git \
     --exclude audit.sh \
+    --exclude *.json \
     -R "$1" .)
   if [ -n "$found" ]; then
     fail
@@ -76,22 +75,98 @@ check_has() {
   fi
 }
 
+check_has_deps() {
+  white "Checking for dependency $1... "
+  # NOTE(termie): govendor list is super slow
+  #found=$(govendor list +vendor | grep "$1")
+  found=$(grep "\"path\"" ./vendor/vendor.json | grep "$1")
+  if [ -z "$found" ]; then
+    fail
+    echo "Did not find $1 in vendor.json"
+    return 1
+  else
+    success
+  fi
+}
+
+# Replacing blueprint with $name,
+diff_wercker_yml() {
+  name=$(jq -r .Name < .managed.json)
+  port=$(jq -r .Port < .managed.json)
+  gateway=$(jq -r .Gateway < .managed.json)
+  echo "$BLUEPRINTDIR"
+  echo "$name"
+  sed -e "s/blueprint/$name/g;s/\/templates\/service//g;s/666/$port/g;s/667/$gateway/g" "$BLUEPRINTDIR/templates/service/wercker.yml" > /tmp/audit.yml
+  git diff -u /tmp/audit.yml wercker.yml
+}
+
+# Check that we output the build artifact
+check_artifact_output() {
+  white "Checking artifact output... "
+  name=$(jq -r .Name < .managed.json)
+  found=$(grep --binary-files=without-match "cp -r \"\$WERCKER_OUTPUT_DIR/$name\" \"\$WERCKER_REPORT_ARTIFACTS_DIR\"" "$1")
+  if [ -z "$found" ]; then
+    fail
+    echo "Did not find artifact copy to output dir"
+    return 1
+  else
+    success
+  fi
+}
+
 main() {
   (
-    cd "$1" || exit
+    cd "$1" || exit 1
+    diff_wercker_yml
     check_has_no glide.*
     check_not_using "github.com/Sirupsen/logrus"
     check_not_using "github.com/codegangsta/cli"
+    check_not_using 2016
     check_has "core/generate-protobuf.sh"
     check_has ".managed.json"
     check_has "version.go"
+    check_has "deployment/deployment.template.yml"
+    check_has_deps "github.com/wercker/pkg/log"
+    check_artifact_output wercker.yml
   )
 }
 
-CHECKDIR=$1
+# Initial values
+WATCH=0
+BLUEPRINTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Check some args
+while [ ! $# -eq 0 ]
+do
+  case "$1" in
+    --watch | -w)
+      shift
+      WATCH=$1
+      ;;
+    *)
+      CHECKDIR=$1
+      ;;
+  esac
+  shift
+done
+
 if [ -z "$CHECKDIR" ]; then
   CHECKDIR=.
 fi
 
-main "$CHECKDIR"
+if [ ! "$WATCH" -eq 0 ]; then
+  clear
+  while true
+  do
+    tput cup 0 0
+    COLS=$(tput cols)
+    printf "Every %ss: audit.sh %s\n" "$WATCH" "$CHECKDIR"
+    tput cup 0 $((COLS-28))
+    date
+    main "$CHECKDIR"
+    sleep "$WATCH"
+  done
+else
+  main "$CHECKDIR"
+fi
 
---
+++ migrate.sh
@@ -0,0 +1,212 @@
+#!/bin/bash
+
+# This is a script to help make automatic changes to a project
+# for blueprint compliance.
+
+# Ideally any changes to the base blueprint code that are automatically
+# updateable would happen in here.
+RED='\033[0;31m'
+GREEN='\033[1;32m'
+WHITE='\033[1;37m'
+NC='\033[0m' # No Color
+
+red() {
+  printf "${RED}$@${NC}"
+}
+
+white() {
+  printf "${WHITE}$@${NC}"
+}
+
+green() {
+  printf "${GREEN}$@${NC}"
+}
+
+walk() {
+  white "Walking with $1...\n"
+  found=$(find . -type f | grep -v vendor | grep -v .wercker | grep -v .git | grep -v .json)
+  for x in $found
+  do
+    if [ "$DEBUG" ]; then
+      echo "  $x"
+    fi
+    eval "$1 $x"
+  done
+}
+
+search_and_replace() {
+  local file=$1
+  local search=$2
+  local replace=$3
+
+  sed -i '' -e "s/$search/$replace/g" "$file"
+}
+
+replace_logrus() {
+  local found=$(grep --binary-files=without-match "logrus" "$1")
+  if [ -z "$found" ]; then
+    # skip anything without logrus
+    return 0
+  fi
+  echo "$found"
+  #read -rp "Press enter to continue"
+
+  search_and_replace "$1" 'log \"github.com\/Sirupsen\/logrus\"' '\"github.com\/wercker\/pkg\/log\"'
+  search_and_replace "$1" '\"github.com\/Sirupsen\/logrus\"' '\"github.com\/wercker\/pkg\/log\"'
+  search_and_replace "$1" 'logrus' 'log'
+
+  goimports -w "$1"
+}
+
+replace_log() {
+  local found=$(grep --binary-files=without-match '"log"' "$1")
+  if [ -z "$found" ]; then
+    # skip anything without logrus
+    return 0
+  fi
+  echo "$found"
+  #read -rp "Press enter to continue"
+
+  search_and_replace "$1" '	\"log\"' '\"github.com\/wercker\/pkg\/log\"'
+
+  goimports -w "$1"
+}
+
+replace_2016() {
+  local found=$(grep --binary-files=without-match "2016" "$1")
+  if [ -z "$found" ]; then
+    # skip anything without logrus
+    return 0
+  fi
+  search_and_replace "$1" "(c) 2016" "(c) 2017"
+  search_and_replace "$1" "(c) 2107" "(c) 2017"
+}
+
+update_golang() {
+  white "Updating govendor step to use CGO_ENABLED...\n"
+  search_and_replace "$1" "golang:1.6" "golang:1.7"
+}
+
+update_govendor_cgo() {
+  white "Updating govendor step to use CGO_ENABLED...\n"
+  search_and_replace "$1" "code: govendor test" "code: CGO_ENABLED=0 govendor test"
+}
+
+update_docker_tags() {
+  white "Updating docker tags to include git commit...\n"
+  local found=$(grep --binary-files=without-match "tag: \$WERCKER_GIT_BRANCH-\$WERCKER_GIT_COMMIT,\$WERCKER_GIT_COMMIT" "$1")
+  if [ -n "$found" ]; then
+    # already there, skip it
+    return 0
+  fi
+  search_and_replace "$1" "tag: \$WERCKER_GIT_BRANCH-\$WERCKER_GIT_COMMIT" "tag: \$WERCKER_GIT_BRANCH-\$WERCKER_GIT_COMMIT,\$WERCKER_GIT_COMMIT"
+}
+
+update_tee_yml() {
+  white "Updating deploy yaml to use tee...\n"
+  local name=$(jq -r .Name < .managed.json)
+  search_and_replace "$1" "cat \*\.yml > $name.yml" "cat *.yml | tee $name.yml"
+}
+
+update_artifact_output() {
+  white "Updating artifact output\n"
+  local name=$(jq -r .Name < .managed.json)
+  local found=$(grep --binary-files=without-match "cp -r \"\$WERCKER_OUTPUT_DIR/$name\" \"\$WERCKER_REPORT_ARTIFACTS_DIR\"" "$1")
+  if [ -n "$found" ]; then
+    # already there, skip it
+    return 0
+  fi
+
+  # NOTE(termie): have mercy
+  search_and_replace "$1" "-o \\\$WERCKER_OUTPUT_DIR\/$name" "-o \\\"\\\$WERCKER_OUTPUT_DIR\/$name\\\""
+  sed -i '' -e "/-o \"\$WERCKER_OUTPUT_DIR\/$name\"/a\\
+\ \ \ \ \ \ \ \ \ \ cp -r \"\$WERCKER_OUTPUT_DIR\/$name\" \"\$WERCKER_REPORT_ARTIFACTS_DIR\"" $1
+}
+
+update_managed_json() {
+  white "Updating .managed.json...\n"
+  # server port
+  local server_port=$(grep -A 1 '"port",' server.go | grep "Value:" | sed -e 's/.* \([0-9]\{4,6\}\),/\1/')
+  local gateway_port=$(grep -A 1 '"port",' gateway.go | grep "Value:" | sed -e 's/.* \([0-9]\{4,6\}\),/\1/')
+  local description=$(grep "app.Usage" main.go | sed -e 's/.*"\(.*\)"/\1/')
+  local name=$(basename "$PWD")
+  local year=2017
+
+  cat <<EOF > .managed.json
+{
+  "Template": "service",
+  "Name": "$name",
+  "Port": ${server_port:-666},
+  "Gateway": ${gateway_port:-667},
+  "Year": "$year",
+  "Description": "$description"
+}
+EOF
+  git diff .managed.json
+}
+
+rename_yaml() {
+  local name=$(jq -r .Name < .managed.json)
+  white "Renaming $name-*.template.yml to *.template.yml...\n"
+  local files=$(ls deployment/$name-*.template.yml 2> /dev/null)
+  if [ -z "$files" ]; then
+    return 0
+  fi
+  for x in $files;
+  do
+    local new_name=$(echo $x | sed -e "s/$name-//")
+    if [ "$DEBUG" ]; then
+      echo "$x -> $new_name"
+    fi
+    git mv "$x" "$new_name"
+  done
+}
+
+ensure_dep() {
+  white "Ensuring go dependency $1...\n"
+  # NOTE(termie): govendor list is super slow
+  #found=$(govendor list +vendor | grep "$1")
+  local found=$(grep "\"path\"" ./vendor/vendor.json | grep "$1")
+  if [ -z "$found" ]; then
+    echo "Did not find $1 in vendor.json, fetching"
+    govendor fetch "$1"
+    return 1
+  fi
+}
+
+main() {
+  (
+  cd "$1" || exit 1
+  update_managed_json
+  rename_yaml
+  walk "replace_logrus"
+  walk "replace_log"
+  ensure_dep "github.com/wercker/pkg/log"
+  walk "replace_2016"
+  update_golang wercker.yml
+  update_govendor_cgo wercker.yml
+  update_docker_tags wercker.yml
+  update_tee_yml wercker.yml
+  update_artifact_output wercker.yml
+  )
+}
+
+# Check some args
+while [ ! $# -eq 0 ]
+do
+  case "$1" in
+    --debug | -d)
+      DEBUG=1
+      ;;
+    *)
+      CHECKDIR=$1
+      ;;
+  esac
+  shift
+done
+
+if [ -z "$CHECKDIR" ]; then
+  CHECKDIR=.
+fi
+
+main $CHECKDIR
